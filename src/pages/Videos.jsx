025ce9c20bbc68717c97b331720c522dd9c692a6
Peter <gudqo1995@naver.com>
2024-04-12 20:47:23 +0900

    get video list

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,6 +1,31 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getVideoList } from "../api/Video";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ["videoList", keyword],
+    queryFn: () => getVideoList(keyword),
+    staleTime: 1000 * 60 * 60,
+  });
+  return (
+    <>
+      <div>Videos {keyword}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id.videoId} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
